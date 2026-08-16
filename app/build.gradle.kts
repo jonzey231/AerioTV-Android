e442cdab51ed06de2fc663875ec0d77482be8aab
Logan Jones <jonzey231@gmail.com>
2026-08-16 14:31:19 -0400

    Bump version to 0.4.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.aeriotv.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 44
-        versionName = "0.4.16"
+        versionCode = 45
+        versionName = "0.4.17"
         vectorDrawables { useSupportLibrary = true }
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
